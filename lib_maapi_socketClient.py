cfd30f069ca9e3827638d4fc90841ad719aa3b74
Sebastian <sebastian.antosiak@gmail.com>
2019-02-04 08:25:29 +0100

    Update lib_maapi_socketClient.py

--- lib_maapi_socketClient.py
+++ lib_maapi_socketClient.py
@@ -6,9 +6,15 @@ class socketClient():
     def __init__(self, *args, **kwargs):
         return super().__init__(*args, **kwargs)
 
-    def sendStr(self,host,port, payload):
+    def sendStrAndRecv(self,host,port, payload):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((host, port))
             s.sendall(bytes("{payload}".format(payload=payload),"utf-8"))
             data = s.recv(1024)
+            s.close()
         return data
+    def sendStr(self,host,port, payload):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((host, port))
+            s.sendall(bytes("{payload}".format(payload=payload),"utf-8"))
+            s.close()
