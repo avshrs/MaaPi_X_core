a45cf3434708ea6e61129deffe2709ab003d68e5
kasia <@>
2019-02-05 13:50:33 +0100

    update

--- lib_maapi_queue.py
+++ lib_maapi_queue.py
@@ -7,6 +7,8 @@
 #
 ##############################################################
 from datetime import datetime as dt
+from threading import RLock
+
 
 class queue():
     def __init__(self):
@@ -15,26 +17,34 @@ class queue():
 
         #self.socketReadings = {"owner": {"fomHost": {"onPort":[{id:["data","reciveTohost","reciveToPort"]]}}} }
         self.socketReadings = {}
-    
+        self.lock = RLock()
+
     def addSocketRadings(self,owner,fomHost,onPort, data, reciveToHost = None, reciveToPort = None, dt_=dt.now()):
-        if not self.socketReadings:
-            data_=[data,reciveToHost,reciveToPort,dt_]
-            id_={} 
-            port_={}
-            host_={}
-            id_[self.seqSRnr]=data_
-            port_[onPort]=id_
-            host_[fomHost]=port_
-            self.socketReadings[owner]=host_
-            #print ("empty",self.socketReadings)
-        else:
-            self.socketReadings[owner][fomHost][onPort][self.seqSRnr]=[data,reciveToHost,reciveToPort,dt_]
-            #print ("adding",self.socketReadings)
-        self.seqSRnr +=1
+        try:
+            self.lock.acquire()
+            if not self.socketReadings:
+                data_=[data,reciveToHost,reciveToPort,dt_]
+                id_={} 
+                port_={}
+                host_={}
+                id_[self.seqSRnr]=data_
+                port_[onPort]=id_
+                host_[fomHost]=port_
+                self.socketReadings[owner]=host_
+                #print ("empty",self.socketReadings)
+            else:
+                self.socketReadings[owner][fomHost][onPort][self.seqSRnr]=[data,reciveToHost,reciveToPort,dt_]
+                #print ("adding",self.socketReadings)
+            self.seqSRnr +=1
+        except:
+            pass
+        finally:
+            self.lock.release()
 
     def getSocketRadings(self):
+        self.lock.acquire()
         return self.socketReadings
-
+        self.lock.release()
     def get_queue_nr(self):
          return self.queue_nr
 
