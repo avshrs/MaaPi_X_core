4277b17eb096a1dc225935fa554ad082308ad922
avshrs <avshrs@gmail.com>
2019-02-05 23:01:59 +0100

    update

--- lib_maapi_socketServer.py
+++ lib_maapi_socketServer.py
@@ -4,7 +4,10 @@ from datetime import datetime as dt
 class SocketServer():
     def __init__(self):
         self.debug = 1
-     
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def __del__(self):
+        self.sock.close()
+
     def _debug(self, level, msg):
         if self.debug >= level:
             print("DEBUG lib socketServer\t\t{0} {1}, {2}".format(level, dt.now(), msg))
@@ -13,15 +16,15 @@ class SocketServer():
     def startServer(self,owner, host, port, queue, object_id):
         try:
             try:
-                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                sock.bind((host, int(port)))
+                #self.sock  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.sock.bind((host, int(port)))
             except:
-                sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                sock.bind((host, int(port)))
-            sock.listen(1000)  
-            self._debug(1,sock)
+                self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                self.sock.bind((host, int(port)))
+            self.sock.listen(1000)  
+            self._debug(1,self.sock)
             while True:
-                client, address = sock.accept()
+                client, address = self.sock.accept()
                 with client:
                     while True:
                         data = client.recv(20000)
@@ -41,10 +44,10 @@ class SocketServer():
             self._debug(1,"---------------------------------------------------")
             self._debug(1,"{e}".format(e=e))
             self._debug(1,"---------------------------------------------------")
-            sock.close()
+            self.sock.close()
 
         finally:
-            sock.close()
+            self.sock.close()
            
    
  
