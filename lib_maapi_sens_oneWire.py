bacd825b2cff1600e9ae93ace6ce65ca817e9225
kasia <@>
2019-02-07 00:04:20 +0100

    update

--- lib_maapi_sens_oneWire.py
+++ lib_maapi_sens_oneWire.py
@@ -14,10 +14,14 @@ import lib_maapi_socketClient               as SocketClient
 import lib_maapi_queue                      as Queue
 import lib_maapi_db_connection              as Db_connection    
 import MaaPi_Config                         as Config
+
 from threading import Lock, Thread
 from datetime import datetime as dt, timedelta
-import time
-import copy
+
+import time, os, copy
+
+
+
 class MaapiSelector():
 
     def __init__(self):
@@ -31,7 +35,7 @@ class MaapiSelector():
         # vars
         self.board_id           = 0
         self.maapiLocation      = self.config.maapiLocation
-        self.objectname         = "selector"
+        self.objectname         = "OneWireModule"
         self.selectorPort       = self.config.selectorPort
         self.selectorHost       = self.config.selectorHost
         self.thread             = []
@@ -56,49 +60,47 @@ class MaapiSelector():
             self.thread.append(Thread(target=self.socketServer.startServer, args=(self.objectname,self.selectorHost, self.selectorPort, self.queue, 1)))
             self.thread[0].start()
 
-
-    def checkDbForOldreadings(self):
-        readings = [1,2,3]
-        return readings
-
-    
-    def scanQueueForIncommingQuerys(self,queue):
+    def scanQueueForSelectorAck(self,queue):
         try:
-            queue__= copy.deepcopy(queue[self.objectname][self.selectorHost][self.selectorPort])
+            if queue[self.objectname][self.selectorHost][self.selectorPort]:    
+                queue__ = copy.deepcopy(queue[self.objectname][self.selectorHost][self.selectorPort])
         except:
             pass
-        else:            
+        else:
             for que in queue__:
-                data = queue__[que][0]
+                data     = queue__[que][0]
                 recvHost = queue__[que][1]
                 recvPort = queue__[que][2]
                 dtime    = queue__[que][3]
-          
-                if data == "is ok?":
-                    self._debug(1,"del inserted data ")
+                self._debug(1,data)
+                if data == "ok":                  
+                    self._debug(1,"get ok from selector")
+                          
                     del queue[self.objectname][self.selectorHost][self.selectorPort][que]
+            return "nn"
 
+   
 
-    def DeviceList(self):
-        board_location = self.maapiDB.table("maapi_machine_locations").filters_eq(ml_enabled=True).get()
-        for i in board_location:
-            if board_location[i]["ml_location"] == self.maapiLocation:
-                self.board_id = board_location[i]["id"]
-        
-        data = self.maapiDB.table("devices").columns(
-                "dev_id",
-                "dev_type_id",
-                "dev_rom_id",
-                "dev_bus_type_id",
-                "dev_last_update",
-                "dev_interval",
-                "dev_interval_unit_id",
-                "dev_interval_queue",
-                "dev_machine_location_id",
-                ).order_by('dev_id').filters_eq(
-                dev_status=True, dev_machine_location_id=self.board_id).get()
+    def read_data_from_1w(self, rom_id, dev_id):
+        if os.path.isfile('/sys/bus/w1/devices/{0}/w1_slave'.format(rom_id)):
+            w1_file = open('/sys/bus/w1/devices/{0}/w1_slave'.format(rom_id),'r')
+            self._debug(1,"Open file /sys/bus/w1/devices/{0}/w1_slave".format(rom_id))
+            w1_line = w1_file.readline()
+            w1_crc = w1_line.rsplit(' ', 1)
+            w1_crc = w1_crc[1].replace('\n', '')
+            if w1_crc == 'YES':
+                self._debug(2, "CRC - YES")
+                w1_temp = w1_file.readline().rsplit('t=', 1)
+                temp = float(float(w1_temp[1]) / float(1000))
+                self._debug(1, "Read_data_from_1w - Value is {0} for rom_id[1] {1}".format(temp, dev_id))
+                w1_file.close()
+                return temp
+            else:
+                return "CRC_NO"
+        else:
+            return "FILE_NOT_EXIST"
 
- 
+    
 
     def SendDataToServer(self,host,port,data):
         try:
@@ -120,3 +122,26 @@ if __name__ == "__main__":
     MaapiSel.runTcpServer()
     MaapiSel.loop() 
 
+def __init__(self, *args):
+        for rom_id in args:
+            condition, condition_min_max, force_value = Check().condition(
+                rom_id[0])
+            self._debug(
+                1,
+                "Condition is = {0}\t condition_min_max is = {1}, \t forced value is = {2}".
+                format(condition, condition_min_max, force_value))
+            if condition:
+                if condition_min_max:
+                    self.read_data_from_1w(rom_id[1], rom_id[0])
+                    
+
+                else:
+
+                    maapidb.MaaPiDBConnection.insert_data(
+                        rom_id[0], force_value, ' ', True)
+                    self._debug(
+                        1,
+                        "Forcing value for sensor id = {0} \tforced vslur is = {1} ".
+                        format(rom_id[0], force_value))
+            else:
+                self.read_data_from_1w(rom_id[1], rom_id[0])
