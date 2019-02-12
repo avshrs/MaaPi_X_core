e60a7914b553826406f169344224084802575ad1
avshrs <avshrs@gmail.com>
2019-02-12 11:05:42 +0100

    update

--- lib_maapi_queue.py
+++ lib_maapi_queue.py
@@ -16,7 +16,7 @@ class queue():
     def __init__(self):
         self.seqSRnr = 0
         self.queue_tcp_radings = {}
-        self.socketReadings =  dict(  dict( dict(  dict( list() ))))
+        self.socketReadings =  {}
         
         self.debug = 1
     
