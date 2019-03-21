8a28c8edb6d91f028a5b7900143ec1d258dd1530
avshrs <avshrs@gmail.com>
2019-03-21 21:33:36 +0100

    update

--- lib_maapi_main_socketServer.py
+++ lib_maapi_main_socketServer.py
@@ -63,7 +63,7 @@ class SocketServer():
             while True:
                 data, address = sockUDP.recvfrom(4096)
                 if not data: break
-                payload_id, dev_id, value, name  = data.split(",")
+                payload_id, dev_id, value, name  = data.decode("utf-8").split(",")
                 if data:
                     if payload_id == "SOCKET":
                         self.queue.addSocketRadings(self.objectname, host, port, payload_id, dev_id, value, name )
