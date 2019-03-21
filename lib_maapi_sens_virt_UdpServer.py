72c0c7c5c6591fd9511381f1f64292cb866d0f60
avshrs <avshrs@gmail.com>
2019-03-21 21:56:11 +0100

    update

--- lib_maapi_sens_virt_UdpServer.py
+++ lib_maapi_sens_virt_UdpServer.py
@@ -34,7 +34,7 @@ class UdpServer():
         self.readings           = Readings.Readings(self.objectname,self.host, self.port)
         self.maapiDB            = Db_connection.MaaPiDBConnection()
         self.socketServer       = SocketServer.SocketServer(self.objectname, self.queue,id_)
-        self.socketServer.runUdpServer(self.host, 50505)
+        self.socketServer.runUdpServer(self.host, self.port)
 
 
     def checkQueueForReadings(self):
