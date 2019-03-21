db69411c750ca62ec60508e1a77684a498a30000
avshrs <avshrs@gmail.com>
2019-03-21 22:24:55 +0100

    update

--- lib_maapi_sens_virt_UdpServer.py
+++ lib_maapi_sens_virt_UdpServer.py
@@ -44,7 +44,7 @@ class UdpServer():
 
         except:
             for nr in queue_:
-                if queue_[nr][0] == self.helpers.instructions["recive_from_UDP"]::
+                if queue_[nr][0] == self.helpers.instructions["recive_from_UDP"]:
                     insert_readings(int(queue_[nr][1]),float(queue_[nr][2])," ",boolean)
                     self.maapilogger.log("INFO",f"Recived id: {nr:<10} DevID: {int(queue_[nr][1]):<8} Name: {'Recive From UDP':<20} \tValue: {float(float(queue_[nr][2]))} ")
 
