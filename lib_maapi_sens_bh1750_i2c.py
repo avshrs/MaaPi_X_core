211d9dd7cc8192c6fc042d1c550e3a3084d8498d
avshrs <avshrs@gmail.com>
2019-03-24 00:49:15 +0100

    update

--- lib_maapi_sens_bh1750_i2c.py
+++ lib_maapi_sens_bh1750_i2c.py
@@ -68,12 +68,13 @@ class BME280I2C():
         # Device is automatically set to Power Down after measurement.
         ONE_TIME_LOW_RES_MODE = 0x23
 
-        bus = smbus.SMBus(1) # Rev 1 Pi uses 0
-        data = bus.read_i2c_block_data(DEVICE,ONE_TIME_HIGH_RES_MODE_1)
+        #bus = smbus.SMBus(1) # Rev 1 Pi uses 0
+        #data = bus.read_i2c_block_data(DEVICE,ONE_TIME_HIGH_RES_MODE_1)
 
-        # bus = I2C_MaaPi(1)  # Rev 2 Pi uses 1
-        # data = bus.read_i2c_block_data(DEVICE, CONTINUOUS_HIGH_RES_MODE_1,)
-        value = ((data[1] + (256 * data[0])) / 1.2)
+        bus = I2C_MaaPi(1)  # Rev 2 Pi uses 1
+        data = bus.read_i2c_block_data(DEVICE, CONTINUOUS_HIGH_RES_MODE_1,32)
+        value = (data[1] + (256 * data[0])) / 1.2
+
         return value, error
 
 
