ac17486a96dcdbe37cb738745dcf0d1f395be08c
avshrs <avshrs@gmail.com>
2019-03-24 11:04:03 +0100

    update - test

---
+++ temp.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+import sys
+from statistics import median, stdev, mean
+from datetime import datetime as dt
+from lim_maapi_i2c_bus import I2C_MaaPi
+
+logging.basicConfig(
+    filename='/home/pi/MaaPi110/bin/logs/Maapi_Selector.log',
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%m/%d/%Y %H:%M:%S')
+
+class class_get_values(object):
+    debug = 1
+    def __init__(self, address, sens, loops):
+        self.addr = address
+        self.loop = loops
+        self.sensor = sens
+
+        self.bus = I2C_MaaPi(1)
+
+
+    def readFromI2C(self,sensor, address, loops):
+        data = []
+        for i in range(0,loops):
+            data.append(self.bus.write_read_i2c_block_data32(self.addr,int(self.sensor),int(self.sensor),loops))
+        print (data)
+        print (f"min \tvalue: {min(data)}")
+        print (f"mean \tvalue: {mean(data)}")
+        print (f"max \tvalue: {max(data)}")
+        print (f"median \tvalue: {median(data)}")
+        print (f"stdev \tvalue: {stdev(data)}")
+
+if __name__ == "__main__":
+    class_get_values_ =  class_get_values(sys.argv[1], sys.argv[2], sys.argv[3])
+    class_get_values_.readFromI2C()
+
+
