a2895a7452dd714854dd3496702ef662f5f1cf1e
avshrs <avshrs@gmail.com>
2019-03-26 22:17:35 +0100

    update

--- start_maapi.sh
+++ start_maapi.sh
@@ -1,4 +1,4 @@
 maapiWatcher="MaaPi_Watcher.py"
 interpreter="python3.6"
 
-nohup python3.6 MaaPi_Watcher.py > /log/Maapi_console.log 2>&1 & echo $! > pid/MaaPi_wacher_sh.pid
+nohup python3.6 MaaPi_Watcher.py > log/Maapi_console.log 2>&1 & echo $! > pid/MaaPi_wacher_sh.pid
