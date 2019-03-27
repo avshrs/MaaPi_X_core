e80d6f68ad0c2c9f3cebcad61b3b123234d016a6
avshrs <avshrs@gmail.com>
2019-03-27 22:48:51 +0100

    update

--- start_maapi.sh
+++ start_maapi.sh
@@ -3,11 +3,16 @@ interpreter="python3.7"
 
 pid=$(cat pid/MaaPi_Watcher.pid)
 
-if ! kill -0 $pid > /dev/null 2>&1; then
-    echo "Starting MaaPi_Watcher"
-    nohup $interpreter $maapiWatcher > log/MaaPi_Watcher.log 2>&1 & echo $!
-    # nohup $interpreter $maapiWatcher > log/MaaPi_Watcher.log 2>&1 & echo $!
+if [ "$1" = "start" ]; then
+    if ! kill -0 $pid > /dev/null 2>&1; then
+        echo "Starting MaaPi_Watcher"
+        nohup $interpreter $maapiWatcher > log/MaaPi_Watcher.log 2>&1 & echo $!
+        # nohup $interpreter $maapiWatcher > log/MaaPi_Watcher.log 2>&1 & echo $!
 
-else
-    echo "MaaPi_Watcher Running"
+    else
+        echo "MaaPi_Watcher Running"
+    fi
+fi
+if [ "$1" = "stop" ]; then
+    kill -9 $pid
 fi
