1865bb11095adddd3334adf633126315a3b1d21d
avshrs <avshrs@gmail.com>
2019-07-04 11:49:08 +0200

    u

--- start_maapi.sh
+++ start_maapi.sh
@@ -6,20 +6,23 @@ loc=$(pwd)
 if [ "$1" = "start" ]; then
     if ! kill -0 $(ps -aux | grep "[M]aaPi_Watcher.py" | awk '{print $2}') > /dev/null 2>&1; then
         echo "Starting MaaPi_Watcher"
-        nohup $interpreter $maapiWatcher >> ${loc}/log/MaaPi_Watcher.log 2>&1 & echo $!
+        nohup $interpreter $maapiWatcher >> /dev/null 2>&1 &
     else
         echo "MaaPi Running"
     fi
-fi
-
-if [ "$1" = "stop" ]; then
-    if ! kill -0 $(ps -aux | grep "[M]aaPi_Watcher.py" | awk '{print $2}') > /dev/null 2>&1; then
-        echo "MaaPi Not Running"
-    else
-        kill -2 $(ps -aux | grep "[M]aaPi_Watcher.py" | awk '{print $2}')
-        sleep 1
+else
+    if [ "$1" = "stop" ]; then
         if ! kill -0 $(ps -aux | grep "[M]aaPi_Watcher.py" | awk '{print $2}') > /dev/null 2>&1; then
-            echo "MaaPi - stoped"
+            echo "MaaPi Not Running"
+        else
+            kill $(ps -aux | grep "[M]aaPi_Watcher.py" | awk '{print $2}')
+            sleep 1
+            if ! kill -0 $(ps -aux | grep "[M]aaPi_Watcher.py" | awk '{print $2}') > /dev/null 2>&1; then
+                echo "MaaPi - stoped"
+            fi
         fi
+    else
+           echo "Use ./start_maapi.sh start|stop"
     fi
+
 fi
